ce90305ffeeb4f246188dff5fd8c05568b9ef9f4
soubhagyasethy <ssethy017@gmail.com>
2023-01-27 22:48:12 +0530

    refactor: added data file and updated props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
 import Header from "./components/header"
 import Card from "./components/card"
+import Data from "./data"
 
 function App() {
+
+  let dataCard = Data.map((item) => {
+    return(
+      <Card item = {item} />
+    )
+  })
   return (
     <div className="App">
       <Header />
       <section className="section-card">
-        <Card
-            image = "../images/mount fuji.jpg"
-            locationName ="JAPAN"
-            locationLink ="/"
-            locationTitle = "Mount Fuji"
-            timeline = "12 Jan, 2021 - 24 Jan, 2021"
-            description = "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-
-        />
+        {dataCard}
       </section>
     </div>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -2,17 +2,17 @@
 export default function Card(props) {
     return(
         <section className="card">
-            <img className="card-image" src={props.image} alt="mount fuji"/>
+            <img className="card-image" src={props.item.image} alt="mount fuji"/>
             <div className="card-content">
                 <div className="location">
                     <img className="location-icon" src="../images/location.svg" alt="location icon"/>
-                    <span className="location-name">{props.locationName}</span>
-                    <a className="location-link" href={props.locationLink}>View on Google Maps</a>
+                    <span className="location-name">{props.item.locationName}</span>
+                    <a className="location-link" href={props.item.locationLink}>View on Google Maps</a>
                 </div>
 
-                <h1 className="location-title">{props.locationTitle}</h1>
-                <p className="timeline">{props.timeline}</p>
-                <p className="description">{props.description}</p>
+                <h1 className="location-title">{props.item.locationTitle}</h1>
+                <p className="timeline">{props.item.timeline}</p>
+                <p className="description">{props.item.description}</p>
             </div>
             
         </section>
