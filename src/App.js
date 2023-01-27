bfa3cd03ec062d01abf5f98113f216a7d471c050
soubhagyasethy <ssethy017@gmail.com>
2023-01-27 22:11:38 +0530

    refactor: added props in card

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ function App() {
       <Header />
       <section className="section-card">
         <Card
-            
+            image = "../images/mount fuji.jpg"
+            locationName ="JAPAN"
+            locationLink ="/"
+            locationTitle = "Mount Fuji"
+            timeline = "12 Jan, 2021 - 24 Jan, 2021"
+            description = "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+
         />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
       </section>
     </div>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -2,17 +2,17 @@
 export default function Card(props) {
     return(
         <section className="card">
-            <img className="card-image" src="../images/mount fuji.jpg" alt="mount fuji"/>
+            <img className="card-image" src={props.image} alt="mount fuji"/>
             <div className="card-content">
                 <div className="location">
                     <img className="location-icon" src="../images/location.svg" alt="location icon"/>
-                    <span className="location-name">JAPAN</span>
-                    <a className="location-link" href="/">View on Google Maps</a>
+                    <span className="location-name">{props.locationName}</span>
+                    <a className="location-link" href={props.locationLink}>View on Google Maps</a>
                 </div>
 
-                <h1 className="location-title">Mount Fuji</h1>
-                <p className="timeline">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h1 className="location-title">{props.locationTitle}</h1>
+                <p className="timeline">{props.timeline}</p>
+                <p className="description">{props.description}</p>
             </div>
             
         </section>
